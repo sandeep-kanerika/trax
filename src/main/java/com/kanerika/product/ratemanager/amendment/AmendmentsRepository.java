d99dad0baa28d964acf6d8f50025289363cbc8a1
sandeep.sable <sandeep.sable>
2019-03-07 14:22:16 +0530

    Added functionality to find the active Amendments.

--- src/main/java/com/kanerika/product/ratemanager/amendment/AmendmentsRepository.java
+++ src/main/java/com/kanerika/product/ratemanager/amendment/AmendmentsRepository.java
@@ -1,5 +1,8 @@
 package com.kanerika.product.ratemanager.amendment;
 
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kanerika.product.ratemanager.jpa.AbstractJpaRepository;
@@ -11,4 +14,8 @@ import com.kanerika.product.ratemanager.jpa.AbstractJpaRepository;
 public interface AmendmentsRepository extends AbstractJpaRepository<Amendments, Long> {
 	
     Amendments getById(Long id);
+    
+    @Query("SELECT u FROM Amendments u WHERE u.status = 1")
+    Collection<Amendments> findAllActiveAmendments();
+    
 }
--- src/main/java/com/kanerika/product/ratemanager/amendment/AmendmentsService.java
+++ src/main/java/com/kanerika/product/ratemanager/amendment/AmendmentsService.java
@@ -1,22 +1,25 @@
 package com.kanerika.product.ratemanager.amendment;
 
-import com.kanerika.product.ratemanager.exception.ResourceNotFoundException;
-import com.kanerika.product.ratemanager.jpa.AbstractJpaRepository;
-import com.kanerika.product.ratemanager.jpa.AbstractJpaService;
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import com.kanerika.product.ratemanager.exception.ResourceNotFoundException;
+import com.kanerika.product.ratemanager.jpa.AbstractJpaRepository;
+import com.kanerika.product.ratemanager.jpa.AbstractJpaService;
 
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by sudhakar.rao on 2/19/2019.
  */
 @Service
+@Slf4j
 public class AmendmentsService extends AbstractJpaService<Amendments> {
 
 	private static final Logger logger = LoggerFactory.getLogger(AmendmentsService.class);
@@ -24,69 +27,83 @@ public class AmendmentsService extends AbstractJpaService<Amendments> {
     @Autowired
     AmendmentsRepository amendmentsRepository;
 
-    @Override
+	@Override
 	public Amendments create(Amendments amendments) {
-        return save(amendments);
-    }
+		log.debug("inside create ammendments");
+		return save(amendments);
+	}
 
-    @Override
-    @Transactional
+	@Override
+	@Transactional
 	public Amendments update(Amendments _amendments) {
-        //Get the data from DB and update the fields and information from request data
-        Amendments amendments = getById(_amendments.getId());
-        if (amendments != null) {
-        	
-        	amendments.setStatus(_amendments.getStatus());
-        	amendments.setReferenceId(_amendments.getReferenceId());
-        	amendments.setType(_amendments.getType());
-        	amendments.setDescription(_amendments.getDescription());
-        	amendments.setRatesetId(_amendments.getRatesetId());
-        	amendments.setRatesetReferenceId(_amendments.getRatesetReferenceId());
-            amendments.setBuyerOrgId(_amendments.getBuyerOrgId());
-            amendments.setSellerOrgId(_amendments.getSellerOrgId());
-            amendments.setBuyerOrgName(_amendments.getBuyerOrgName());
-            amendments.setSellerOrgName(_amendments.getSellerOrgName());
-            amendments.setRatesetName(_amendments.getRatesetName());
-            amendments.setRegion(_amendments.getRegion());
-            amendments.setMode(_amendments.getMode());
-            amendments.setDefaultEffectiveDateFrom(_amendments.getDefaultEffectiveDateFrom());
-            amendments.setDefaultEffectiveDateThru(_amendments.getDefaultEffectiveDateThru());
-            amendments.setReviewedBy(_amendments.getReviewedBy());
-            amendments.setCreatedBy(_amendments.getCreatedBy());
-            amendments.setLastUpdatedBy(_amendments.getLastUpdatedBy());
-            amendments.setLastAssignedBy(_amendments.getLastAssignedBy());
-            amendments.setApprovers(_amendments.getApprovers());
-            amendments.setCurrentApprover(_amendments.getCurrentApprover());
-            amendments.setDateApproved(_amendments.getDateApproved());
-            amendments.setDateCreated(_amendments.getDateCreated());
-            amendments.setDateUpdated(_amendments.getDateUpdated());
-            amendments.setDateReviewed(_amendments.getDateReviewed());
-            amendments.setDateAssigned(_amendments.getDateAssigned());
-            
-            return save(amendments);
-        } else {
-            throw new ResourceNotFoundException("Amendment Id Not Found !");
-        }
-    }
+		// Get the data from DB and update the fields and information from request data
+		Amendments amendments = getById(_amendments.getId());
+		if (amendments != null) {
 
-    public Amendments getById(Long id) {
-//		return search(id);
-    	return amendmentsRepository.getById(id);
-    }
+			amendments.setStatus(_amendments.getStatus());
+			amendments.setReferenceId(_amendments.getReferenceId());
+			amendments.setType(_amendments.getType());
+			amendments.setDescription(_amendments.getDescription());
+			amendments.setRatesetId(_amendments.getRatesetId());
+			amendments.setRatesetReferenceId(_amendments.getRatesetReferenceId());
+			amendments.setBuyerOrgId(_amendments.getBuyerOrgId());
+			amendments.setSellerOrgId(_amendments.getSellerOrgId());
+			amendments.setBuyerOrgName(_amendments.getBuyerOrgName());
+			amendments.setSellerOrgName(_amendments.getSellerOrgName());
+			amendments.setRatesetName(_amendments.getRatesetName());
+			amendments.setRegion(_amendments.getRegion());
+			amendments.setMode(_amendments.getMode());
+			amendments.setDefaultEffectiveDateFrom(_amendments.getDefaultEffectiveDateFrom());
+			amendments.setDefaultEffectiveDateThru(_amendments.getDefaultEffectiveDateThru());
+			amendments.setReviewedBy(_amendments.getReviewedBy());
+			amendments.setCreatedBy(_amendments.getCreatedBy());
+			amendments.setLastUpdatedBy(_amendments.getLastUpdatedBy());
+			amendments.setLastAssignedBy(_amendments.getLastAssignedBy());
+			amendments.setApprovers(_amendments.getApprovers());
+			amendments.setCurrentApprover(_amendments.getCurrentApprover());
+			amendments.setDateApproved(_amendments.getDateApproved());
+			amendments.setDateCreated(_amendments.getDateCreated());
+			amendments.setDateUpdated(_amendments.getDateUpdated());
+			amendments.setDateReviewed(_amendments.getDateReviewed());
+			amendments.setDateAssigned(_amendments.getDateAssigned());
 
-    @Override
-    @Transactional
-    protected Amendments delete(Amendments amendments) {
-        return save(amendments);
-    }
+			return save(amendments);
+		} else {
+			throw new ResourceNotFoundException("Amendment Id Not Found !");
+		}
+	}
 
-    @Override
-    protected List<Amendments> search(Amendments amendments) {
-        return null;
-    }
+	public Amendments getById(Long id) {
+		return amendmentsRepository.getById(id);
+	}
 
-    @Override
-    protected AbstractJpaRepository<Amendments, Long> getRepository() {
-		return amendmentsRepository ;
-    }
+	@Override
+	@Transactional
+	protected Amendments delete(Amendments amendments) {
+		amendmentsRepository.delete(amendments);
+		return amendments;
+	}
+	
+	public Boolean delete(Long id)
+	{
+		try
+		{
+			amendmentsRepository.deleteById(id);
+			return true;
+		}
+		catch(Exception ex)
+		{
+			return false;
+		}
+	}
+
+	@Override
+	protected List<Amendments> search(Amendments amendments) {
+		return amendmentsRepository.findAll();
+	}
+
+	@Override
+	protected AbstractJpaRepository<Amendments, Long> getRepository() {
+		return amendmentsRepository;
+	}
 }
