d750946e840ec1e192d67cb5ea2e42aa204f2608
sandeep.sable <sandeep.sable@traxtech.com>
2019-03-19 13:48:41 +0530

    application.properties modified.

--- src/main/java/com/trax/ratemanager/column/validation/ColumnValidation.java
+++ src/main/java/com/trax/ratemanager/column/validation/ColumnValidation.java
@@ -1,9 +1,12 @@
 package com.trax.ratemanager.column.validation;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.trax.ratemanager.ratecolumn.RateColumn;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,35 +20,25 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ColumnValidation {
-
 	@Id
 	private String id;
-
+	private String columnDefinitionId;
 	private String concept;
-
-	private String name;
-
-	private Long sortOrder;
-
-	private String type;
-
-	private String dateSourceId;
-
-	private String dateFieldId;
-
-	private Boolean hasAlias;
-
-	private Boolean isKey;
-
-	private String reference;
-
-	private String pivotField;
-
-	private String displayName;
-
-	private String propertyName;
-
-	private String pivotCharge;
-
+	private String dataFieldId;
+	private String dataSourceId;
 	private String description;
+	private String displayName;
+	private Boolean isActive;
+	private Boolean isRequired;
+	private String name;
+	private String pivotCharge;
+	private String pivotField;
+	private String propertyName;
+	private Integer sortyOrder;
+	private String type;
+	private String validValues;
+	private String validationType;
+
+	@OneToOne(mappedBy = "validation", fetch = FetchType.LAZY)
+	private RateColumn column;
 }
